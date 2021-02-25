5ad048ce5b456f9714ea909f035308a6d2c9a25e
discolarrence <discolarrence@gmail.com>
2021-02-24 22:09:50 -0500

    changed divs & ids to be targeted for modals. added js to show bio on click, but still doesn't work iterating through array of beetle names

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-// show beetle bio text on click
+// show beetle bio modal on image click
+debugger;
+const bigGirlImg = document.querySelector('#big-girl-img');
+const bigGirlModal = document.querySelector('#big-girl-modal');
+const blackbuttImg = document.querySelector('#blackbutt-img');
+const blackbuttModal = document.querySelector('#blackbutt-modal');
+const theOtherOneImg = document.querySelector('#the-other-one-img');
+const theOtherOneModal = document.querySelector('#the-other-one-modal');
+const newGirlImg = document.querySelector('#new-girl-img');
+const newGirlModal = document.querySelector('#new-girl-modal');
+const sicklyTwinImg = document.querySelector('#sickly-twin-img');
+const sicklyTwinModal = document.querySelector('#sickly-twin-modal');
+const strongTwinImg = document.querySelector('#strong-twin-img');
+const strongTwinModal = document.querySelector('#strong-twin-modal');
 
+const beetles = ["bigGirl", "blackbutt", "theOtherOne", "newGirl", "sicklyTwin", "strongTwin"];
 
-const beetles = ['big-girl', 'blackbutt', 'the-other-one', 'new-girl', 'sickly-twin', 'strong-twin'];
-    //beetle bio container div classes'
+for (i=0; i<beetles.length; i++) {
+    let beetleImg = beetles[i] + "Img";
+    let beetleModal = beetles[i] + "Modal";
+    beetleImg.addEventListener('click', () => {
+        beetleModal.style.display = 'block';
+})}
 
-let bioImg = document.getElementsByClassName('bio-img');
-
-bioImg.addEventListener('click', showBioText());
-    //returns bio imgs and listens for click event to show text
-
-function showText() {
-    const text = document.querySelector("bioClass");
-    text.style["display"] = "block";
-};
-    //sets display to block for bio text
-
-  
-
-
-
-
+// bigGirlImg.addEventListener('click', () => {
+//    bigGirlModal.style.display = 'block';
+// })
 
