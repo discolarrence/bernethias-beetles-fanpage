573db6c9d71ce1e1a7aab8a2fe097fd1d3104984
discolarrence <discolarrence@gmail.com>
2021-03-03 20:55:05 -0500

    condensed js to show hidden modals

--- script.js
+++ script.js
@@ -1,27 +1,13 @@
 // show beetle bio modal on image click
-const bigGirlImg = document.querySelector('#big-girl-img');
-const bigGirlModal = document.querySelector('#big-girl-modal');
-const blackbuttImg = document.querySelector('#blackbutt-img');
-const blackbuttModal = document.querySelector('#blackbutt-modal');
-const theOtherOneImg = document.querySelector('#the-other-one-img');
-const theOtherOneModal = document.querySelector('#the-other-one-modal');
-const newGirlImg = document.querySelector('#new-girl-img');
-const newGirlModal = document.querySelector('#new-girl-modal');
-const sicklyTwinImg = document.querySelector('#sickly-twin-img');
-const sicklyTwinModal = document.querySelector('#sickly-twin-modal');
-const strongTwinImg = document.querySelector('#strong-twin-img');
-const strongTwinModal = document.querySelector('#strong-twin-modal');
-
-const beetles = [{clickImg: bigGirlImg, box: bigGirlModal}, 
-    {clickImg: blackbuttImg, box: blackbuttModal}, 
-    {clickImg: theOtherOneImg, box: theOtherOneModal}, 
-    {clickImg: newGirlImg, box: newGirlModal},
-    {clickImg: sicklyTwinImg, box: sicklyTwinModal},
-    {clickImg: strongTwinImg, box: strongTwinModal}];
-
-for (i=0; i<beetles.length; i++) {
-    let beetle = beetles[i];
-    beetle.clickImg.addEventListener('click', () => {
-    beetle.box.style.display = 'block';
-})}
+const bioImgs = document.querySelectorAll('.bio-img-container img');
+const bioImgModals = document.querySelectorAll('.bio-modal');
+for (i=0; i<bioImgs.length; i++) {
+bioImgs[i].addEventListener('click', (event) => {
+    for (i=0; i<bioImgModals.length; i++) {
+        bioImgModals[i].style.display = 'none';
+    }
+    let beetleImg = document.getElementById(event.target.className+'-modal');
+        beetleImg.style.display = 'block';
+});
+}
 
