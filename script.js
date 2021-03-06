677fbdc9c30341517d24ad4e62003d8f93cf54ed
discolarrence <discolarrence@gmail.com>
2021-03-06 13:12:09 -0500

    added close icon & script to close bio-modals

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-// show beetle bio modal on image click
+debugger;
+//define variables
 const bioImgs = document.querySelectorAll('.bio-img-container img');
 const bioImgModals = document.querySelectorAll('.bio-modal');
-for (i=0; i<bioImgs.length; i++) {
-bioImgs[i].addEventListener('click', (event) => {
-    for (i=0; i<bioImgModals.length; i++) {
+const xCloseIcons = document.querySelectorAll('.x-close-icon');
+
+// show beetle bio modal on image click
+function closeBioModal() {
+    for (i = 0; i<bioImgModals.length; i++) {
         bioImgModals[i].style.display = 'none';
-    }
-    let beetleImg = document.getElementById(event.target.className+'-modal');
-        beetleImg.style.display = 'block';
+}};
+
+for (i=0; i<bioImgs.length; i++) {
+    bioImgs[i].addEventListener('click', (event) => {
+        closeBioModal();
+        let beetleImg = document.getElementById(event.target.className+'-modal');
+        beetleImg.style.display = 'flex';
 });
 }
 
+//hide beetle bio modal on close icon click
+for (i = 0; i<xCloseIcons.length; i++) {
+    xCloseIcons[i].addEventListener('click', (event) => {  
+        closeBioModal();
+});
+};
