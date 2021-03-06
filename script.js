8cc8047e6c8c5ed9fd89247764799001afa0bc54
discolarrence <discolarrence@gmail.com>
2021-03-06 15:14:20 -0500

    added script to close modal on click abywhere. styled & centered modal

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
-debugger;
+
+
 //define variables
 const bioImgs = document.querySelectorAll('.bio-img-container img');
 const bioImgModals = document.querySelectorAll('.bio-modal');
 const xCloseIcons = document.querySelectorAll('.x-close-icon');
 
-// show beetle bio modal on image click
+//close bio modals
 function closeBioModal() {
     for (i = 0; i<bioImgModals.length; i++) {
         bioImgModals[i].style.display = 'none';
 }};
 
+// show beetle bio modal on image click
 for (i=0; i<bioImgs.length; i++) {
     bioImgs[i].addEventListener('click', (event) => {
         closeBioModal();
-        let beetleImg = document.getElementById(event.target.className+'-modal');
+        let beetleImg = document.getElementById(event.target.id+'-modal');
         beetleImg.style.display = 'flex';
-});
-}
-
-//hide beetle bio modal on close icon click
-for (i = 0; i<xCloseIcons.length; i++) {
-    xCloseIcons[i].addEventListener('click', (event) => {  
-        closeBioModal();
-});
+    });
 };
+
+// //hide bio modal on click anywhere
+document.addEventListener('click', (event) => { 
+    if (event.target.className !== 'bio-img' && 'bio-modal') {
+        closeBioModal();
+    };
+})
+
+// document.addEventListener('click', (event) => {
+//     closeBioModal();
+//     for (i=0; i<bioImgs.length; i++) {
+//         if (event.target.id == bioImgs[i]) {
+//             let beetleImg = document.getElementById(event.target.id+'-modal');
+//             beetleImg.style.display = 'flex';
+//         }
+//     };
+//     for (i=0; i<bioImgModals.length; i++) {
+//         if (event.target.className == 'bio-modal'){
+//            let beetleModal = document.getElementById(event.target.id);
+//             beetleModal.style.display = 'flex'; 
+//         }
+//     };
+//     if (event.target.className == 'x-close-icon'){
+//             closeBioModal();
+//         }  
+//     })
